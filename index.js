1aa68be7e6ce28903faf04ddb3289499531b9258
creatorio <rdjunior018@gmail.com>
2024-10-08 12:22:33 +0530

    Added API No sleep

--- index.js
+++ index.js
@@ -86,3 +86,24 @@ app.get("/", (req, res) => {
 app.listen(port, function () {
   console.log(`server is running on port ${port}`);
 });
+const url = "https://rentapp-backend-3pnf.onrender.com/"; // Replace with your Render URL
+const interval = 300000 * 3; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  fetch(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
